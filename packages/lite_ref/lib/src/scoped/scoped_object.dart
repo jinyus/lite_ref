36f8d8b051b8360f90f3d19b1dcbc4f4c11ef8c3
jinyus <starboymc4@gmail.com>
2024-04-19 12:45:39 -0500

    Add copy method to ScopedObject

--- packages/lite_ref/lib/src/scoped/scoped_object.dart
+++ packages/lite_ref/lib/src/scoped/scoped_object.dart
@@ -5,7 +5,6 @@ part of 'scoped.dart';
 /// If [autoDispose] is set to `true`, the instance will be disposed when
 /// all the widgets that have access to the instance are unmounted.
 class ScopedObject<T> {
-
   /// Creates a new [ScopedObject] with an [id] and an [instance].
   ScopedObject({
     required Object id,
@@ -27,6 +26,10 @@ class ScopedObject<T> {
   final DisposeFn<T>? _onDispose;
 
   int _watchCount = 0;
+
+  ScopedObject<T> _copy() {
+    return ScopedObject(id: _id, instance: _instance);
+  }
 
   void _dispose() {
     if (_instance == null) return;
